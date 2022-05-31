6baa714f22763ec256eb0fd627eefc6da62642fc
zwh2119 <1548767361@qq.com>
2022-05-31 13:38:44 +0800

    add device initial algo

--- test_debug.py
+++ test_debug.py
@@ -3,5 +3,4 @@ import json
 if __name__ == '__main__':
     with open('al/algo.json', 'r') as f:
         algo_list = json.load(f)
-        a = json.dumps(algo_list)
-        print(a)
+        print(list(algo_list.keys())[0])
